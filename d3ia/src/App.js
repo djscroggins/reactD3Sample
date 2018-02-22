52778f74d5b0af88078f78dcfb1211e8197f451e
David <>
2018-02-22 13:56:13 -0600

    Adding map and stream graph

--- d3ia/src/App.js
+++ d3ia/src/App.js
@@ -2,16 +2,38 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import BarChart from './BarChart';
+import WorldMap from './WorldMap';
+import worlddata from './world';
+import StreamGraph from './StreamGraph';
+import {range, sum} from 'd3-array';
+import {scaleThreshold} from 'd3-scale';
+import {geoCentroid} from 'd3-geo';
+
+const appdata = worlddata.features.filter(d => geoCentroid(d)[0] < -20);
+
+appdata.forEach((d, i) => {
+    const offset = Math.random();
+    d.launchday = i;
+    d.data = range(30).map((p, q) => q < i ? 0 : Math.random() * 2 + offset);
+});
 
 class App extends Component {
     render() {
+
+        const colorScale = scaleThreshold().domain([5, 10, 20, 30, 50])
+            .range(["#75739F", "#5EAFC6", "#41A368", "#93C464", "#FE9922"]);
+
+
         return (
             <div className="App">
                 <div className="App-header">
                     <h2>d3ia dashboard</h2>
                 </div>
                 <div>
-                    <BarChart data={[5, 10, 1, 3]} size={[500, 500]}/>
+                    <StreamGraph colorScale={colorScale} data={appdata} size={[1000, 250]}/>
+                    <WorldMap colorScale={colorScale} data={appdata} size={[500, 400]}/>
+                    <BarChart colorScale={colorScale} data={appdata} size={[500, 400]}/>
+
                 </div>
 
             </div>
